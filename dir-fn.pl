0ebd828f508702e44a1da1fd0ac67a8e5d861a36
oggy8021 <oggyist@gmail.com>
2009-05-16 04:21:43 +0000

    git-svn-id: file:///home/katsu/svn/dir-fn/trunk@5 dda265a8-8553-40fc-b93e-4a03d6c46144

--- dir-fn.pl
+++ dir-fn.pl
@@ -4,27 +4,82 @@
 
 
 use File::Basename;
-use File::Find;
+use Jcode;
 
 #----------------------
 
-@search_dirs = $ARGV[0];
+$arg_dir = $ARGV[0];
 
-find(\&mp3files, @search_dirs);
+&start($arg_dir);
 
-sub mp3files
+sub start
 {
-	next if (($_ eq '.') or ($_ eq '..'));
-	next if ($_ =~ m/^(_Single|_single|Single)$/);
+	local($dir) = $_[0];
 
-	$fname =  $File::Find::name;
+	$dir =~ s/\s/\\ /g;
+	$dir = ".";
+
+	opendir(DIR, $dir) || die("Can not Open Directory : $dir");
+	my @dir = readdir(DIR);
+	closedir(DIR);
+
+	foreach (@dir)
+	{
+#		printf "$dir\n";		##DEBUG
+		next if (($_ eq '.') or ($_ eq '..'));
+		next if ($_ =~ m/^(_Single|_single|Single)$/);
+		if (-d $_) {
+#			print "$_ is Dir\n";		##DEBUG
+			&sub_dir($_);
+		} else {
+#			print "$_ is File\n";		##DEBUG
+			&sub_file($_);
+		}
+	}
+}
+
+sub sub_dir
+{
+	local($dir) = $_[0];
+	
+	chdir $dir || die("Can not Change Directory : $dir");
+	print "$dir に入ります\n";
+	&start($dir);
+	print "$dir を出ます\n";
+	chdir "..";
+}
+
+sub sub_file
+{
+	local($file) = $_[0];
+	
+#	print "$fileです \n";		##DEBUG
+	$fname = basename($file);
 	$cname = $fname;
+#	$bname = basename($file, ".mp3");
 
+	#rename
 	$cname =~ s/\(/[/g;
 	$cname =~ s/\)/]/g;
 	$cname =~ s/'//g;
 	$cname =~ s/\s/_/g;
-#	print "$fname => $cname\n";		##DEBUG
-	print "===> $cname\n";		##DEBUG
+	$sjname = &esc_filename($cname);
+#	print "\t$fname => $cname, $sjname\n";		##DEBUG
+	print "\t$fname => $sjname\n";		##DEBUG
 #	rename($fname, $cname) || die("Can not change File Name");
+
 }
+
+sub esc_filename
+{
+	local($filename) = $_[0];
+
+#	my $mcode = 'sjis'; ## sjis, euc, jis
+	my $mcode = 'euc';
+#	my $forbedden = '\\\/\*\?\|"<>:,;% ';
+#	my $filename = @_;
+	$filename = Jcode::convert($filename, $mcode);
+#	$filename =~ s/([$forbedden])/'%' . unpack('H2', $1)/eg;
+	return $filename;
+}
+
