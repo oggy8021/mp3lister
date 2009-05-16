da36a7afc48c139a8d86de21ea2d057bb28aad56
oggy8021 <oggyist@gmail.com>
2009-05-16 04:20:56 +0000

    git-svn-id: file:///home/katsu/svn/dir-fn/trunk@3 dda265a8-8553-40fc-b93e-4a03d6c46144

--- dir-fn.pl
+++ dir-fn.pl
@@ -16,53 +16,12 @@ sub start
 	local($dir) = $_[0];
 
 	$dir =~ s/\s/\\ /g;
-	$dir = ".";
 
 	opendir(DIR, $dir) || die("Can not Open Directory : $dir");
 	my @dir = readdir(DIR);
-	closedir(DIR);
-
-	foreach (@dir)
-	{
-#		printf "$dir\n";		##DEBUG
-		next if (($_ eq '.') or ($_ eq '..'));
-		next if ($_ =~ m/^(_Single|_single|Single)$/);
-		if (-d $_) {
-#			print "$_ is Dir\n";		##DEBUG
-			&sub_dir($_);
-		} else {
-#			print "$_ is File\n";		##DEBUG
-			&sub_file($_);
-		}
+	foreach (@dir) {
+		print "$_\n";
 	}
+	closedir(DIR);
 }
 
-sub sub_dir
-{
-	local($dir) = $_[0];
-	
-	chdir $dir || die("Can not Change Directory : $dir");
-	print "$dir に入ります\n";
-	&start($dir);
-	print "$dir を出ます\n";
-	chdir "..";
-}
-
-sub sub_file
-{
-	local($file) = $_[0];
-	
-#	print "$fileです \n";		##DEBUG
-	$fname = basename($file);
-	$cname = $fname;
-#	$bname = basename($file, ".mp3");
-
-	#rename
-	$cname =~ s/\(/[/g;
-	$cname =~ s/\)/]/g;
-	$cname =~ s/'//g;
-	$cname =~ s/\s/_/g;
-	print "\t$fname => $cname\n";		##DEBUG
-#	rename($fname, $cname) || die("Can not change File Name");
-
-}
